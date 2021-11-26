481403f3e5d4fc51cbdee6c58a93a4b02f537479
Tanya <kistrinatanya906@gmail.com>
2021-11-26 10:47:22 +0300

    цвета орехов

---
+++ enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Linq;
+
+namespace ооп
+{
+    enum Color
+    {
+        Red,
+        Green,
+        Orange,
+        Yellow,
+        Blue,
+        Brown,
+        LightBrown
+    }
+}

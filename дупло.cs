b702e48c5cc2f10a04e1f7b290faf9c41677a2c2
Tanya <kistrinatanya906@gmail.com>
2021-11-26 10:50:01 +0300

    class hollow

---
+++ дупло.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using System.Linq;
+
+namespace ооп
+{
+     class hollow
+    {
+        public hollow()
+        {
+            nuts = new nut[] { };
+        }
+
+
+        public string name;
+        public nut[] nuts;
+        private int x;
+}
